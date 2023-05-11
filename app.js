b2bc357479637d0a04af8e3bd88d9a09ebffb0e6
Sahil Sorathiya <67726151+Sahil-Sorathiya@users.noreply.github.com>
2023-05-11 11:07:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { data } = require('./temp_data.js') // String
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // Setting template engine
-app.set('views', path.join(__dirname, '/.netlify/functions/views'))
+app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 
 // Routes 
